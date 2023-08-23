003fa9defc6f7e1270a14934d7e0fdef57666542
DuduGaspar <142595025+DuduGaspar@users.noreply.github.com>
2023-08-23 20:11:05 -0300

    Add files via upload

---
+++ C/Caixa Registradora.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+float preco, total;
+char resp[1];
+int x, y;
+main(){
+printf("!CAIXA REGISTRADORA!\n\n");
+while(x<1){
+	printf("Deseja adicionar um produto?\nDigite: S = Sim ou N = Não\n");
+	while(y<1){
+		gets(resp);
+		if(strcmp(resp,"N")!=0 && strcmp(resp,"S")!=0){
+			printf("Digite S ou N! Letra Maiuscula!!!");
+		} else {
+			y=1;
+		}
+	}
+	if(strcmp(resp,"N")==0){
+		x=1;
+	}
+	if(strcmp(resp,"S")==0){
+	printf("Digite o preço do produto:\n");
+	scanf("%f",&preco);
+	total=total+preco;
+	}
+}
+printf("Total a pagar = %0.2f",total);
+}
